49991cbba3be657358814a6814dc7d1e02007ffa
aa2il <jattili@yahoo.com>
2023-04-07 16:13:48 -0700

    Updates

---
+++ windoz.bat
@@ -0,0 +1,15 @@
+@echo off
+echo.
+echo Notes about how to run oscope gui on Windoze 10
+echo.
+echo Havent really execised this yet but seems ok so far
+echo    oscope.py
+echo.
+echo This does spew a bunch of warnings that I should look at
+echo.
+echo To compile (Haven't tried this yet)
+echo.
+echo         pyinstaller --onefile oscope.py
+echo         dist\oscpe.exe
+echo.
+
